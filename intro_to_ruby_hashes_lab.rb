be5d2c8dc5c77ee0ebdcfe6928885a2285a231d6
George Apetrei <george.apetrei@gmail.com>
2019-12-16 20:05:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,11 +26,11 @@ return hash[key]
 end
 
 def update_counting_hash(hash, key)
-  hash=[key]
+  hash[key]
 if hash[key]
-  return hash[key]+=1
+  hash[key]+=1
   else
- return  hash[key]=1
+  hash[key]=1
 end
-return hash
+p hash
 end
